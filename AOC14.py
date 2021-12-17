36184f07825eb3a9bc843b35adacfb2ef0b5bfb6
CMyers79 <codermyers@gmail.com>
2021-12-16 23:33:59 -0800

    Day 14 complete

--- AOC14.py
+++ AOC14.py
@@ -1,33 +1,60 @@
-import string
-
-
 with open('input14.txt') as file:
     lines = file.readlines()
-    rules = []
+    rules = {}
     template = lines[0].strip()
-    tempset = set()
-    chardist = []
+    chardict = {}
+    tempdict = {}
+    stepdict = {}
     for char in template:
-        tempset.add(char)
-
+        chardict[char] = 0
 
     for row in range(2, len(lines)):
-        rules.append(lines[row].strip().split())
+        rules[lines[row].strip().split()[0]] = lines[row].strip().split()[2]
 
-    for _ in range(10):
+    for pair in rules:
+        pairlist = [pair]
 
-        insert = []
-        for i in range(len(template) - 1):
-            for j in range(len(rules)):
-                if template[i] + template[i + 1] == rules[j][0]:
-                    insert.append([i + 1, rules[j][2]])
+        for step in range(20):
+            start = len(pairlist)
+            for char in chardict:
+                tempdict[char] = 0
+            for i in range(start - 1, -1, -1):
+                pairlist.append(pairlist[i][0] + rules[pairlist[i]])
+                pairlist.append(rules[pairlist[i]] + pairlist[i][1])
+                tempdict[pairlist[i][0]] += 1
+                tempdict[pairlist[i][1]] += 1
+                tempdict[rules[pairlist[i]]] += 2
+            del pairlist[:start]
 
-        for i in range(len(insert)):
-            template = template[:insert[i][0]] + insert[i][1] + template[insert[i][0]:]
-            for j in range(i + 1, len(insert)):
-                insert[j][0] += 1
+        if (pair, step + 1) not in stepdict:
+            stepdict[(pair, step + 1)] = {char: tempdict[char] for char in tempdict}
 
-    for char in tempset:
-        chardist.append(template.count(char))
+    print(stepdict)
 
-    print(max(chardist) - min(chardist))
+    pairs = [template[i] + template[i + 1] for i in range(len(template) - 1)]
+
+    for pair in pairs:
+        pairlist = [pair]
+
+        for step in range(20):
+            start = len(pairlist)
+            for char in chardict:
+                tempdict[char] = 0
+            for i in range(start - 1, -1, -1):
+                pairlist.append(pairlist[i][0] + rules[pairlist[i]])
+                pairlist.append(rules[pairlist[i]] + pairlist[i][1])
+                tempdict[pairlist[i][0]] += 1
+                tempdict[pairlist[i][1]] += 1
+                tempdict[rules[pairlist[i]]] += 2
+            del pairlist[:start]
+
+        for p in pairlist:
+            for char in chardict:
+                chardict[char] += stepdict[(p, 20)][char]
+
+    chardict[template[0]] += 1
+    chardict[template[-1]] += 1
+    for char in chardict:
+        chardict[char] /= 2
+
+    print(max(chardict.values()) - min(chardict.values()))
---
+++ AOC15.py
@@ -0,0 +1,2 @@
+with open('input15.txt') as file:
+    lines = file.readlines()
